2b1bcb44db2d631744e5956835ccd10070b0606d
guarin98 <guarin98@gamil.com>
2023-08-18 11:44:07 -0500

    nuevos archivos

---
+++ prueba1.py
@@ -0,0 +1,8 @@
+a = int (input "entre nuemero")
+b = int (input "entre numero2")
+suma = a+b
+s = str(suma)
+print ("la suma es: " +s)
+
+
+
---
+++ terreno.py
@@ -0,0 +1,11 @@
+print ("TERRENO")
+lado1=15
+lado2=5
+perimetro= lado1+lado1+lado2+lado2
+area=lado1*lado2
+print ("=============================================")
+print ("el perimetro es:  ",perimetro)
+print ("el área es:  ",area)
+print ("=============================================")
+
+      
